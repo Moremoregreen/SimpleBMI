de80910b8c9ef6ccf4b80b45d7f86bdb05586ee2
Moremoregreen <zzz2009j@gmail.com>
2018-05-13 16:41:03 +0800

    改用StringBuilder

--- app/src/main/java/com/moremoregreen/a4_1bmi/ResultActivity.java
+++ app/src/main/java/com/moremoregreen/a4_1bmi/ResultActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.text.NumberFormat;
+
 public class ResultActivity extends AppCompatActivity {
     private TextView tvBMI, tvResult;
     private Button btnBack;
@@ -17,19 +19,41 @@ public class ResultActivity extends AppCompatActivity {
 
         findViews();
         Bundle bundle = this.getIntent().getExtras();
-        Double height = bundle.getDouble("Height");
-        Double weight = bundle.getDouble("Weight");
-        Double BMI = weight / height / height;
-        tvBMI.setText("BMI：" + BMI);
+        if(bundle != null) {
+            Double height = bundle.getDouble("Height");
+            Double weight = bundle.getDouble("Weight");
 
-        if (BMI >= 24){
-            tvResult.setText("太胖囉!");
-        }else if(BMI <= 18.5){
-            tvResult.setText("太瘦了哦!!");
-        }else if (height <= 0 || weight <= 0){
-            tvResult.setText("請重新輸入身高體重");
-        }else {
-            tvResult.setText("你不錯!身材很標準");
+            if (height >= 3) {
+                height /= 100;
+            }
+            Double BMI = weight / height / height;
+
+            tvBMI.setText("BMI：" + BMI);
+
+//            if (BMI >= 24)
+//                tvResult.setText("太胖囉!");
+//             else if (BMI <= 18.5)
+//                tvResult.setText("太瘦了哦!!");
+//             else if (height <= 0 || weight <= 0)
+//                tvResult.setText("請重新輸入身高體重");
+//             else
+//                tvResult.setText("你不錯!身材很標準");
+            StringBuilder sb = new StringBuilder();
+            NumberFormat nf = NumberFormat.getInstance();
+            nf.setMaximumFractionDigits(2);
+            String bmiStr = nf.format(BMI);
+            String str;
+            if (BMI >= 24)
+            str = "太胖囉!";
+             else if (BMI <= 18.5)
+                str = "太瘦了哦!!" ;
+            else if (height <= 0 || weight <= 0)
+                str = "請重新輸入身高體重" ;
+            else
+                str = "你不錯!身材很標準";
+
+            sb.append("BMI：").append(bmiStr).append("\n").append(str);
+            tvResult.setText(sb);
         }
     }
 
